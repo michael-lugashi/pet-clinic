f31aed125ec0622fe8695be139df90aab47e62f2
michael-lugashi <michael.lugashi@gmail.com>
2025-10-31 23:47:45 +0200

    Initial commit: Pet Clinic project

---
+++ components/pet-clinic-dashboard.tsx
@@ -0,0 +1,15 @@
+import { Title } from './title';
+
+export const PetClinicDashboard = () => {
+  return (
+    // Change whatever you want here. It's just an example of using tailwind
+    <div className="grid grid-rows-auto-1fr gap-y-4 p-4 md:p-8 max-w-screen-lg mx-auto">
+      <Title />
+      <Table />
+    </div>
+  );
+};
+
+const Table = () => {
+  return <div>Dashboard Table</div>;
+};
---
+++ components/title.tsx
@@ -0,0 +1,6 @@
+export const Title = () => {
+  return (
+    <h1 className="text-primary font-bold text-3xl">Pet Clinic Dashboard</h1>
+  );
+};
+
