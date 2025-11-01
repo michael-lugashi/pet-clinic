05292b88a6025c5cdedfaaf8ac3ff34ae3f373f4
michael-lugashi <michael.lugashi@gmail.com>
2025-11-01 21:36:14 +0200

    added temp patients api

--- pages/api/patients.ts
+++ pages/api/patients.ts
@@ -1,14 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { IPatient } from "@/lib/interfaces";
+import { Patient } from "@/lib/interfaces";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 interface PatientResult {
-  pateints?: Array<IPatient> | IPatient;
+  patients?: Array<Patient> | Patient;
 }
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<PatientResult>,
+  res: NextApiResponse<PatientResult | { error: string }>,
 ) {
   if (req.method === "POST") {
     return await create(req, res);
@@ -23,15 +23,49 @@ export default async function handler(
 
 const create = async (req: NextApiRequest, res: NextApiResponse) => {
   // insert to database
-  const patient: IPatient = { name: "Alice" };
+  const patient: Patient = {
+    id: 1,
+    clientName: "Alice Smith",
+    petName: "Max",
+    phone: "1234567890",
+    petAge: "5 years",
+    petType: "Dog",
+    
+  };
   const patients = [patient];
   res.status(200).json({ patients });
 };
 const read = async (req: NextApiRequest, res: NextApiResponse) => {
   // load from database
-  const patients = [{ name: "Boc" }, { name: "Charlie" }];
-  res.status(200).json({ patients });
+  await wait(1000);
+  try {
+    const patients: Patient[] = [
+      {
+        id: 1,
+        clientName: "John Doe",
+        petName: "Boc",
+        phone: "1234567890",
+        petAge: "10 years",
+        petType: "Dog",
+      },
+      {
+        id: 2,
+        clientName: "Jane Doe",
+        petName: "Charlie",
+        phone: "1234567890",
+        petAge: "5 years",
+        petType: "Cat",
+      },
+    ];
+    res.status(200).json({ patients });
+  } catch (error) {
+    console.error(error);
+    res.status(500).end();
+  }
+
 };
 const del = async (req: NextApiRequest, res: NextApiResponse) => {
   // delete from database
 };
+
+const wait = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
