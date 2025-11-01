6842ed6eda1263e4bf26eda49e4ab455f056412e
michael-lugashi <michael.lugashi@gmail.com>
2025-11-01 21:34:54 +0200

    added patient type

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -1,4 +1,10 @@
-export interface IPatient {
-  name: string;
-  // ...
+export type PetType = 'Dog' | 'Cat' | 'Bird' | 'Fish' | 'Other';
+
+export interface Patient {
+  id: number;
+  clientName: string;
+  petName: string;
+  phone: string;
+  petAge: string;
+  petType: PetType;
 }
