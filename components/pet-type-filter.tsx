770f021443d47c9f531123b15500a94bd08556f3
michael-lugashi <michael.lugashi@gmail.com>
2025-11-09 13:29:43 +0200

    add refs to filter and search

--- components/pet-type-filter.tsx
+++ components/pet-type-filter.tsx
@@ -1,37 +1,44 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import Filter from "svg/Filter";
 import { PetType } from "@/lib/interfaces";
-import DropdownSelect, { DropdownOption } from "./dropdown-select";
+import DropdownSelect, { DropdownOption, DropdownSelectRef } from "./dropdown-select";
 
 interface PetTypeFilterProps {
   selectedType: PetType | null;
   onSelectType: (type: PetType | null) => void;
   availableTypes: PetType[];
+  onOpenChange?: (isOpen: boolean) => void;
 }
 
-const PetTypeFilter = ({ selectedType, onSelectType, availableTypes }: PetTypeFilterProps) => {
-  const options: DropdownOption<PetType>[] = availableTypes.map((type) => ({
-    value: type,
-    label: type,
-  }));
+const PetTypeFilter = forwardRef<DropdownSelectRef, PetTypeFilterProps>(
+  ({ selectedType, onSelectType, availableTypes, onOpenChange }, ref) => {
+    const options: DropdownOption<PetType>[] = availableTypes.map((type) => ({
+      value: type,
+      label: type,
+    }));
 
-  const trigger = (
-    <>
-      <Filter className="h-5 w-5" />
-      <span>Pet Type</span>
-    </>
-  );
+    const trigger = (
+      <>
+        <Filter className="h-5 w-5" />
+        <span>Pet Type</span>
+      </>
+    );
 
-  return (
-    <DropdownSelect
-      options={options}
-      selectedValue={selectedType}
-      onSelect={onSelectType}
-      trigger={trigger}
-      showAllOption={true}
-      allOptionLabel="All Types"
-    />
-  );
-};
+    return (
+      <DropdownSelect
+        ref={ref}
+        options={options}
+        selectedValue={selectedType}
+        onSelect={onSelectType}
+        trigger={trigger}
+        showAllOption={true}
+        allOptionLabel="All Types"
+        onOpenChange={onOpenChange}
+      />
+    );
+  }
+);
+
+PetTypeFilter.displayName = "PetTypeFilter";
 
 export default PetTypeFilter;
