ffc2adcb7019ab04818b44615e563de044eafdfd
michael-lugashi <michael.lugashi@gmail.com>
2025-11-09 11:01:19 +0200

    added missing sort type

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -1,5 +1,7 @@
 export type PetType = 'Dog' | 'Cat' | 'Bird' | 'Fish' | 'Other';
 
+export type SortDirection = "asc" | "desc" | null;
+
 export interface Patient {
   id: number;
   clientName: string;
