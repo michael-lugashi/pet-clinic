fe0b1558133721a0caaf06d6ee1f60d7e7446266
michael-lugashi <michael.lugashi@gmail.com>
2025-11-09 10:36:00 +0200

    added svgs

---
+++ svg/SortAsc.tsx
@@ -0,0 +1,9 @@
+const SortAsc = ({ className }: { className?: string }) => {
+  return (
+    <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 15l7-7 7 7" />
+    </svg>
+  );
+};
+
+export default SortAsc;
---
+++ svg/SortDesc.tsx
@@ -0,0 +1,9 @@
+const SortDesc = ({ className }: { className?: string }) => {
+  return (
+    <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+    </svg>
+  );
+};
+
+export default SortDesc;
